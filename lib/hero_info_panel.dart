06c3d49167c9613a0b7c77387982303fcdb9aed6
LefrutFly <voronov-2001@inbox.ru>
2022-10-31 18:13:28 +0600

    [ADD] links in hero_list

--- lib/hero_info_panel.dart
+++ lib/hero_info_panel.dart
@@ -14,53 +14,58 @@ class HeroInfoPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Hero(
         tag: 'heroPanel',
-            child: Scaffold(
-              body: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(urlImage),
-                  ),
-                ),
-                child: Stack(children: <Widget>[
-                  Container(
-                    alignment: Alignment.topLeft,
-                    padding: const EdgeInsets.all(20),
-                    child: IconButton(
-                      onPressed: onTap,
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  //todo: сделать скейлинг текста от размера экрана
-                  Padding(
-                    padding: const EdgeInsets.all(40),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        verticalDirection: VerticalDirection.up,
-                        children: <Widget>[
-                          Text(
-                            heroList[index].info,
-                            style: h2TextStyle,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            heroList[index].name,
-                            style: h1TextStyle,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ]),
+        child: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(heroList[index].linkToInfoPanelImage),
               ),
             ),
+            child: Stack(children: <Widget>[
+              Container(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.all(20),
+                child: IconButton(
+                  onPressed: onTap,
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    verticalDirection: VerticalDirection.up,
+                    children: <Widget>[
+                      FittedBox(
+                        child: Text(
+                          heroList[index].info,
+                          style: h2TextStyle,
+                        ),
+                      ),
+                      const FittedBox(
+                        child: SizedBox(
+                          height: 10,
+                        ),
+                      ),
+                      FittedBox(
+                        child: Text(
+                          heroList[index].name,
+                          style: h1TextStyle,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
       );
 }
--- lib/hero_list.dart
+++ lib/hero_list.dart
@@ -2,22 +2,20 @@ class HeroData {
   final String name;
   final String linkToImage;
   final String info;
+  final String linkToInfoPanelImage;
 
-  const HeroData(this.name, this.linkToImage, this.info);
+  const HeroData(this.name, this.linkToImage, this.info, this.linkToInfoPanelImage);
 }
 
-//todo: поменять данные в linkToImage на ссылки, так как по заданию они должны загружаться из сети
-const captainAmerica = HeroData('Captain America', 'assets/capAmerica.jpg', 'I am Captain America');
-const deadpool = HeroData('Deadpool', 'assets/deadpool.jpg', 'I am Deadpool'); //todo: поменять картинку дедпула, чтобы не было белого фона
-const doctorStrange = HeroData('Doctor Strange', 'assets/doctorStrange.jpg', 'I am Doctor Strange');
-const ironMan = HeroData('Iron Man', 'assets/ironman.jpg', 'I am Iron Man');
-const spiderman = HeroData('Spiderman', 'assets/spiderMan.jpg', 'I am Spiderman');
-const thanos = HeroData('Thanos', 'assets/tanos.jpg', 'I am Thanos');
-const thor = HeroData('Thor', 'assets/tor.jpg', 'I am Thor');
+const captainAmerica = HeroData('Captain America', 'assets/capAmerica.jpg', 'I am Captain America', 'https://gamerwall.pro/uploads/posts/2021-12/1638495974_90-gamerwall-pro-p-supergeroi-kapitan-amerika-marvel-krasivie-99.jpg');
+const doctorStrange = HeroData('Doctor Strange', 'assets/doctorStrange.jpg', 'I am Doctor Strange', 'https://wallpapershome.ru/images/wallpapers/doktor-strendj-720x1280-benedikt-kamberbetch-luchshie-filmi-12210.jpg');
+const ironMan = HeroData('Iron Man', 'assets/ironman.jpg', 'I am Iron Man', 'https://images.hdqwalls.com/download/iron-man-2018-5k-5m-720x1280.jpg');
+const spiderman = HeroData('Spiderman', 'assets/spiderMan.jpg', 'I am Spiderman', 'https://phonoteka.org/uploads/posts/2021-07/1625758898_18-phonoteka-org-p-novii-chelovek-pauk-art-krasivo-19.jpg');
+const thanos = HeroData('Thanos', 'assets/tanos.jpg', 'I am Thanos', 'https://images.hdqwalls.com/download/thanos-ready-for-fight-4k-vk-720x1280.jpg');
+const thor = HeroData('Thor', 'assets/tor.jpg', 'I am Thor', 'https://i.pinimg.com/originals/de/00/f0/de00f05e5851383e9fe05a070947633f.jpg');
 
 const List<HeroData> heroList = [
   captainAmerica,
-  deadpool,
   doctorStrange,
   ironMan,
   spiderman,
